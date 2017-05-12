2cdc09c41ed8b6b0525c3889d90e3208abaaf47a
aktywnitu <aktywnitu@laptop>
2017-05-12 05:41:57 +0200

    Zmiana domyślnej wagi na 2kg i poprawki.

--- data/form_filler.js
+++ data/form_filler.js
@@ -1,16 +1,18 @@
+$("#country").val('217').change();
 var address = self.options.address[0];
 if (address[0] != "") {
     $("#addressName").val(address[0]).change();
 }
 $("#addressLastName").val(address[1]).change();
 $("#addressStreet").val(address[2]).change();
-$("#postCodeSingle").val(address[3]).change();
+$("#postCodeSingle").val(address[3]).change()
+$("#postCodeSingle").val(address[3]).blur();
 $("#addressCity").val(address[4]);
 
 
 $("#textArea").val(self.options.contents[0][0].toLowerCase()).change();
 //domyślna waga paczki - 5kg
-$("#pdl\\[0\\]\\.weight").val('5').change();
+$("#pdl\\[0\\]\\.weight").val('2').change();
 if (self.options.payment) {
     $('#elem_20').click();
     $('#param_20_4').val(self.options.payment[0][0]).change();
